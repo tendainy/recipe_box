f36a94551292bedeffd984213a0db9f2d7a5977d
tendainy <93656026+tendainyandoro@users.noreply.github.com>
2023-02-16 16:37:59 +0200

    Create routes
    Create Recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+# Recipes controller
 class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  private
+
+  def find_recipe
+    @recipes = Recipe.find(params[:id])
+  end
 end
